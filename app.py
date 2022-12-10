09b81bb4ff955ad1155342a9af97499a51bbf479
DotanElse <dotanelse@gmail.com>
2022-12-10 22:43:18 +0200

    database sketch support

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,8 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+def submit_user():
+    conn = sqlite3.connect('users.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+        email text,
+        hashed_password text,
+        first_name text,
+        last_name text,
+        birthday text
+        )
+        """
+        )
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
         
-
-
