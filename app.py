473a31d720fc3015c05f175c4789e24fd2a1fd24
DotanElse <dotanelse@gmail.com>
2022-12-19 23:32:03 +0200

    Support for login and register fully working with database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -10,6 +10,10 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 def submit_user(email, password, name, date):
     conn = sqlite3.connect('users.db')
@@ -29,19 +33,33 @@ def submit_user(email, password, name, date):
         c.execute("INSERT INTO users VALUES (:email, :hashed_password, :name, :birthday)",
         {'email': email, 'hashed_password': password, 'name': name, 'birthday': date})
 
-def register_user():
-    #TODO - get form data
-    #TODO - check if valid input
-    #submit_user with the details
-    pass
-
-def get_user(email):
+def get_user(email, password):
     conn = sqlite3.connect('users.db')
     with conn:
         c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE email=?", (email,))
+        c.execute("SELECT * FROM users WHERE email=? AND hashed_password=?", (email, password))
         return c.fetchall()
+
+@app.route('/process_user_login', methods=['POST'])
+def process_login_form():
+    email = request.form['email']
+    password = request.form['password']
+    # TODO add check of hashing password
+    if get_user(email, password):
+        return render_template("main_page.html")
+    return render_template("failed_login.html")
+
+@app.route('/process_user_register', methods=['POST'])
+def process_register_form():
+  name = request.form['name']
+  email = request.form['email']
+  password = request.form['password']
+  birthday = request.form['birthday']
+  # TODO add hashing of password 
+  submit_user(email, password, name, birthday)
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    print("a")
         
