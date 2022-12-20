1ca1ce3937e9d1e0fad53dd35f3ee80dc2a9b775
DotanElse <dotanelse@gmail.com>
2022-12-20 23:57:48 +0200

    Untested logic for showing user related polls

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ def login():
     return render_template('login.html')
 
 def submit_user(email, password, name, date):
-    conn = sqlite3.connect('users.db')
-    with conn:
-        c = conn.cursor()
+    users_conn = sqlite3.connect('users.db')
+    with users_conn:
+        c = users_conn.cursor()
         # create the db if does not exist, maybe move to different function
         c.execute("""
         CREATE TABLE IF NOT EXISTS users 
@@ -34,20 +34,43 @@ def submit_user(email, password, name, date):
         {'email': email, 'hashed_password': password, 'name': name, 'birthday': date})
 
 def get_user(email, password):
-    conn = sqlite3.connect('users.db')
-    with conn:
-        c = conn.cursor()
+    users_conn = sqlite3.connect('users.db')
+    with users_conn:
+        c = users_conn.cursor()
         c.execute("SELECT * FROM users WHERE email=? AND hashed_password=?", (email, password))
         return c.fetchall()
+
+def show_main_page(email):
+    group_conn = sqlite3.connect('group_conn')
+    user_groups = []
+    with group_conn:
+        c = group_conn.cursor()
+        c.execute("SELECT * FROM groups WHERE email=?")
+        user_groups = c.fetchall()
+
+    # Build the query string for getting all polls related to specific user
+    placeholders = ', '.join(['?'] * len(user_groups))
+    query = f"SELECT * FROM polls WHERE group IN ({placeholders});"
+
+    polls_conn = sqlite3.connect('polls.db')
+    polls = []
+    with polls_conn:
+        c = polls_conn.cursor()
+        c.execute(query, user_groups)
+        polls = c.fetchall()
+    return render_template("main_page.html", email, polls)
+    #TODO - create that database and function to add votes to it
+
 
 @app.route('/process_user_login', methods=['POST'])
 def process_login_form():
     email = request.form['email']
     password = request.form['password']
     # TODO add check of hashing password
-    if get_user(email, password):
-        return render_template("main_page.html")
-    return render_template("failed_login.html")
+    if not get_user(email, password):
+        return render_template("failed_login.html")
+    return show_main_page(email)
+    
 
 @app.route('/process_user_register', methods=['POST'])
 def process_register_form():
