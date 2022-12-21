077e8da69d91c11925189367dac6b40c045212d9
DotanElse <dotanelse@gmail.com>
2022-12-21 23:27:31 +0200

    Removed "groups" database and logic fixes

--- app.py
+++ app.py
@@ -15,53 +15,6 @@ def register():
 def login():
     return render_template('login.html')
 
-def submit_user(email, password, name, date):
-    users_conn = sqlite3.connect('users.db')
-    with users_conn:
-        c = users_conn.cursor()
-        # create the db if does not exist, maybe move to different function
-        c.execute("""
-        CREATE TABLE IF NOT EXISTS users 
-        (
-            email TEXT UNIQUE NOT NULL,
-            hashed_password TEXT NOT NULL,
-            name TEXT NOT NULL,
-            birthday TEXT NOT NULL
-        )
-        """)
-        # add the user
-        c.execute("INSERT INTO users VALUES (:email, :hashed_password, :name, :birthday)",
-        {'email': email, 'hashed_password': password, 'name': name, 'birthday': date})
-
-def get_user(email, password):
-    users_conn = sqlite3.connect('users.db')
-    with users_conn:
-        c = users_conn.cursor()
-        c.execute("SELECT * FROM users WHERE email=? AND hashed_password=?", (email, password))
-        return c.fetchall()
-
-def show_main_page(email):
-    group_conn = sqlite3.connect('group_conn')
-    user_groups = []
-    with group_conn:
-        c = group_conn.cursor()
-        c.execute("SELECT * FROM groups WHERE email=?")
-        user_groups = c.fetchall()
-
-    # Build the query string for getting all polls related to specific user
-    placeholders = ', '.join(['?'] * len(user_groups))
-    query = f"SELECT * FROM polls WHERE group IN ({placeholders});"
-
-    polls_conn = sqlite3.connect('polls.db')
-    polls = []
-    with polls_conn:
-        c = polls_conn.cursor()
-        c.execute(query, user_groups)
-        polls = c.fetchall()
-    return render_template("main_page.html", email, polls)
-    #TODO - create that database and function to add votes to it
-
-
 @app.route('/process_user_login', methods=['POST'])
 def process_login_form():
     email = request.form['email']
@@ -69,7 +22,7 @@ def process_login_form():
     # TODO add check of hashing password
     if not get_user(email, password):
         return render_template("failed_login.html")
-    return show_main_page(email)
+    # should be (return show_main_page(email)), using diff for creating db and testing creation of poll
     
 
 @app.route('/process_user_register', methods=['POST'])
@@ -81,6 +34,59 @@ def process_register_form():
   # TODO add hashing of password 
   submit_user(email, password, name, birthday)
   return render_template('index.html')
+
+def submit_user(email, password, name, date):
+    users_conn = sqlite3.connect('users.db')
+    with users_conn:
+        c = users_conn.cursor()
+        # create the db if does not exist, maybe move to different function
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS users 
+        (
+            email TEXT UNIQUE NOT NULL,
+            hashed_password TEXT NOT NULL,
+            name TEXT NOT NULL,
+            birthday TEXT NOT NULL,
+            groups TEXT
+        )
+        """)
+        # add the user, all users are in the "0" group, which is THE WORLD
+        c.execute("INSERT INTO users VALUES (:email, :hashed_password, :name, :birthday, :groups)",
+        {'email': email, 'hashed_password': password, 'name': name, 'birthday': date, 'groups': "0"})
+
+def get_user(email, password):
+    users_conn = sqlite3.connect('users.db')
+    with users_conn:
+        c = users_conn.cursor()
+        c.execute("SELECT * FROM users WHERE email=? AND hashed_password=?", (email, password))
+        a = c.fetchall()
+        print(a[0][2])
+        return c.fetchall()
+
+def get_user_polls(email):
+    """"returns all polls"""
+    pass
+
+def show_main_page(email):
+    users_conn = sqlite3.connect('users.db')
+    user_groups = []
+    with users_conn:
+        c = users_conn.cursor()
+        c.execute("SELECT * FROM groups WHERE email=?")
+        user_groups = c.fetchall()
+
+    # Build the query string for getting all polls related to specific user
+    placeholders = ', '.join(['?'] * len(user_groups))
+    query = f"SELECT * FROM polls WHERE group IN ({placeholders});"
+    polls_conn = sqlite3.connect('polls.db')
+    polls = []
+    with polls_conn:
+        c = polls_conn.cursor()
+        c.execute(query, user_groups)
+        polls = c.fetchall()
+    #TODO return list of tuples, check code to see how to Enum properly over it
+    return render_template("main_page.html", email, polls)
+    #TODO - create that database and function to add votes to it
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
