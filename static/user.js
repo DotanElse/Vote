22c28827445bea3db3e61148f8b318b7b8b549d7
DotanElse <dotanelse@gmail.com>
2023-03-03 14:15:33 +0200

    User simplified view created

--- static/user.js
+++ static/user.js
@@ -1,3 +1,14 @@
 console.log("-User page-")
 console.log(user)
 console.log(is_owner)
+
+const dateString = user[3]; // date string in "YYYY-MM-DD" format
+
+// Split the string into an array using the (-) as a delimiter
+const dateArray = dateString.split("-");
+
+// Create a new date string in the "DD/MM/YYYY" format using template literals
+const newDateString = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+
+let birthday_paragraph = document.getElementById('profile-birthday');
+birthday_paragraph.textContent = newDateString;
